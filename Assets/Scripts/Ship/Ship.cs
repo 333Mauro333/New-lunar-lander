5b5d09488c60364c49e5ac301628e036c41633ba
Mauro Torres Velasquez <maurotv50@gmail.com>
2022-04-26 17:28:00 -0300

    Mejorado del código
    El código de la nave fue emprolijado.

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 namespace NewLunarLander
 {
+    enum DIRECTION { LEFT, RIGHT }
 
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(MovementControls))]
@@ -12,7 +13,7 @@ namespace NewLunarLander
     public class Ship : MonoBehaviour
     {
         [SerializeField] float maxSpeedTolerance = 5.0f;
-        [SerializeField] float speed = 10.0f;
+        [SerializeField] float accelerationSpeed = 10.0f;
         [SerializeField] float rotationSpeed = 90.0f;
 
         Rigidbody rb;
@@ -20,41 +21,26 @@ namespace NewLunarLander
 
         float actualSpeed = 0.0f;
 
+
         void Awake()
         {
             rb = GetComponent<Rigidbody>();
             mc = GetComponent<MovementControls>();
         }
 
-        // Update is called once per frame
+
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.isKinematic = false;
-            }
-            if (Input.GetKey(mc.GetKey(CONTROLS.UP)))
-            {
-                Accelerate();
-            }
-            if (Input.GetKey(mc.GetKey(CONTROLS.LEFT)))
-            {
-                RotateLeft();
-            }
+            PlayerInput();
 
-            if (Input.GetKey(mc.GetKey(CONTROLS.RIGHT)))
-            {
-                RotateRight();
-            }
-
-            actualSpeed = rb.velocity.y;
+            SaveActualSpeed();
         }
 
         void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.CompareTag("Floor"))
             {
-                if (actualSpeed < -maxSpeedTolerance)
+                if (IsTooFast())
                 {
                     Debug.Log("Se pasó. Velocidad: " + -actualSpeed);
                 }
@@ -66,22 +52,58 @@ namespace NewLunarLander
         }
 
 
+        void SaveActualSpeed()
+        {
+            actualSpeed = rb.velocity.y;
+        }
+        
+
+        void PlayerInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.isKinematic = false;
+            }
+
+            if (Input.GetKey(mc.GetKey(CONTROLS.UP)))
+            {
+                Accelerate();
+            }
+            if (Input.GetKey(mc.GetKey(CONTROLS.LEFT)))
+            {
+                Rotate(DIRECTION.LEFT);
+            }
+            if (Input.GetKey(mc.GetKey(CONTROLS.RIGHT)))
+            {
+                Rotate(DIRECTION.RIGHT);
+            }
+        }
+
         void Accelerate()
         {
-            rb.AddForce(transform.up * speed);
+            rb.AddForce(transform.up * accelerationSpeed);
         }
-        void RotateLeft()
+        void Rotate(DIRECTION direction)
         {
-            Vector3 v3Speed = new Vector3(0.0f, 0.0f, rotationSpeed) * Time.deltaTime;
+            Vector3 v3Speed = Vector3.zero;
+
+            switch (direction)
+            {
+                case DIRECTION.LEFT:
+                    v3Speed = new Vector3(0.0f, 0.0f, rotationSpeed) * Time.deltaTime;
+                    break;
+
+                case DIRECTION.RIGHT:
+                    v3Speed = new Vector3(0.0f, 0.0f, -rotationSpeed) * Time.deltaTime;
+                    break;
+            }
 
             rb.MoveRotation(rb.rotation * Quaternion.Euler(v3Speed));
-            //rb.AddTorque(rotationSpeed);
         }
-        void RotateRight()
-        {
-            Vector3 v3Speed = new Vector3(0.0f, 0.0f, -rotationSpeed) * Time.deltaTime;
 
-            rb.MoveRotation(rb.rotation * Quaternion.Euler(v3Speed));
+        bool IsTooFast()
+        {
+            return actualSpeed < -maxSpeedTolerance;
         }
     }
 }
