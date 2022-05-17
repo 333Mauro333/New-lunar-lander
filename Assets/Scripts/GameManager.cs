4aa70d8b4b9f637570bd6c233c3ee01dd6876090
Mauro Torres Velasquez <maurotv50@gmail.com>
2022-05-17 17:04:46 -0300

    Mejorado del código del GameManager
    El GameManager pasó a ser la clase que controla las escenas.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace NewLunarLander
+{
+    class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        public int score;
+        public bool won;
+
+        public enum SCENE { MENU, GAMEPLAY };
+
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+
+
+        public void LoadGameplay()
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        void Initialize()
+        {
+            // Si no hay una instancia del SceneController...
+            if (instance == null)
+            {
+                // Este controlador se convierte en la escena.
+                instance = this;
+
+                // No me destruyo al cargar la escena.
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                // Para qué quiero crearme si ya hay una instancia de mí? Me destruyo.
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+}
